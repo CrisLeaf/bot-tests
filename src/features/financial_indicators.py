e4986fe3feca6d3f342d63ba3dd3656bec697df3
Cristobal <ctorres@dataqu.cl>
2024-10-23 23:28:22 -0300

    agregada la función que calcula EMA (exponencial) y las bandas de bollinger

--- src/features/financial_indicators.py
+++ src/features/financial_indicators.py
@@ -9,3 +9,23 @@ def calculate_sma_columns(
         df[f'SMA{str(window)}'] = df['close'].rolling(window=window).mean()
     
     return df
+
+def calculate_ema_columns(
+    df: pd.DataFrame,
+    windows: list[int],
+):
+    for window in windows:
+        df[f'EMA{str(window)}'] = df['close'].ewm(span=window, adjust=False).mean()
+    
+    return df
+
+def calculate_bollinger_bands(
+    df: pd.DataFrame,
+    windows: list[int],
+):
+    for window in windows:
+        df[f'BB_Middle_{str(window)}'] = df['close'].rolling(window=window).mean()
+        df[f'BB_Upper_{str(window)}'] = df[f'BB_Middle_{str(window)}'] + 2 * df['close'].rolling(window=window).std()
+        df[f'BB_Lower_{str(window)}'] = df[f'BB_Middle_{str(window)}'] - 2 * df['close'].rolling(window=window).std()
+    
+    return df
